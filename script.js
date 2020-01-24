d84eee18abeef2fcd53004096ab5266e0b2aeaac
Olivia Letendart <letendartolivia@gmail.com>
2020-01-24 10:04:42 +0100

    create two const, one player + one video

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* Get our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+
+/* Build out functions */
+
+/* Hook up the event listeners */
